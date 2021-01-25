944bbd601709c79897ccfeec152f64652eed101e
laura21091984 <77207276+laura21091984@users.noreply.github.com>
2021-01-25 20:55:09 +0100

    ejercicio2

--- Ejercicio2/index.js
+++ Ejercicio2/index.js
@@ -3,7 +3,7 @@
 const  liber  =  {
   día : 21 ,
   mes : 'Septiembre'
-} ;
+};
 consola.log ("Mi cumpleaños es el${liber.día}de${liber.mes}");
 
 const  meses  =  [ 'Enero' ,  'Febrero' ,  'Marzo' ,  'Abril' ,  'Mayo' ,  'Junio' ,  'Julio' ,  'Agosto' ,  'Septiembre' ,  'Octubre' ,  'Noviembre' ,  'Diciembre ' ] ;
