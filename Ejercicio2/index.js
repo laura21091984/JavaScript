7e338644c0c3546bb8f263e3cb4c54eaa7eaea86
laura21091984 <77207276+laura21091984@users.noreply.github.com>
2021-01-25 20:52:12 +0100

    ejercicio2

--- Ejercicio2/index.js
+++ Ejercicio2/index.js
@@ -4,7 +4,7 @@ const  liber  =  {
   día : 21 ,
   mes : 'Septiembre'
 } ;
-consola.log (`Mi cumpleaños es el${liber.día}de${liber.mes}`);
+consola.log ("Mi cumpleaños es el${liber.día}de${liber.mes}");
 
 const  meses  =  [ 'Enero' ,  'Febrero' ,  'Marzo' ,  'Abril' ,  'Mayo' ,  'Junio' ,  'Julio' ,  'Agosto' ,  'Septiembre' ,  'Octubre' ,  'Noviembre' ,  'Diciembre ' ] ;
 
