34a97800468432a543bf010cfedeefcc66f9e350
laura21091984 <77207276+laura21091984@users.noreply.github.com>
2021-01-28 18:58:10 +0100

    Update main.js

--- Ejercicio19/main.js
+++ Ejercicio19/main.js
@@ -1,32 +1,32 @@
 // Al hacer click sobre el botón de Guardar salta un alert
-documento.querySelector ( ".save" ) . addEventListener ( "clic" ,  saveData );
+document.querySelector ( ".save" ) . addEventListener ( "click" ,  saveData );
 
 function  saveData ( )  {
   alert ( 'Se han guardado los datos' );
 };
 
 // Al quitar el foco del input cambia de color
-const  inputName  =  documento . querySelector ( " .nombre " );
+const  inputName  =  document . querySelector ( " .nombre " );
 
-inputName.addEventListener ( 'foco' ,  changeColorFocus );
+inputName.addEventListener ( 'focus' ,  changeColorFocus );
 function  changeColorFocus ( )  {
-  inputName . estilo . backgroundColor  =  '# fff666';
+  inputName.style.backgroundColor  =  '# fff666';
 };
 
 inputName.addEventListener ( 'focusout' ,  changeColorNoFocus );
 function  changeColorNoFocus ( )  {
-  inputName.estilo.backgroundColor  =  '# 9999FF';
+  inputName.style.backgroundColor  =  '# 9999FF';
 };
 
 // Si la letra que pulsemos es vocal lo pintaremos de rojo y de verde si es consonante
-const  inputRandom  =  documento . querySelector ( '.random' );
+const  inputRandom  =  document.querySelector ( '.random' );
 
 inputRandom.addEventListener ( 'pulsación de tecla' ,  checkLetter ) ;
-function  checkLetter ( evento ) {
-  const  currentLetter  =  evento . cual ;
+function  checkLetter ( event ) {
+  const  currentLetter  =  event. cual ;
   if  ( currentLetter  ===  97  ||  currentLetter  ===  101  ||  currentLetter  ===  105  ||  currentLetter  ===  111  ||  currentLetter  ===  117 )  {
-    inputRandom . estilo . color  =  '# a8323e' ;
-  }  más  {
-    inputRandom . estilo . color  =  '# 32a836' ;
+    inputRandom.style.color  =  '# a8323e' ;
+  }  else  {
+    inputRandom.style.color  =  '# 32a836' ;
   }
 } ;
